90eade3def662957c61b6a06efe89f2fcc657676
Yassine94110 <yassineouhmmou@gmail.com>
2022-12-05 00:21:13 +0100

    testmenu

---
+++ testmenu/crudmenu.c
@@ -0,0 +1,133 @@
+#include "menu.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void menuParametreMonstre(){
+	int choix;
+    
+    do {
+    system("clear");
+        printf("\nAJOUTER => 1\n");
+        printf("SUPPRIMER => 2\n");
+        printf("MODIF => 3\n");
+        printf("RETOUR => 4\n");
+        printf("QUITTER => 5\n");
+         printf("Votre choix =>:");
+        scanf("%d", &choix);
+    }while(choix != 1 && choix != 2 && choix != 3 && choix != 4 && choix != 5);
+system("clear");
+    switch( choix )
+    {
+
+        case 1:
+          menuParametreMonstre();
+            break;
+        case 2:
+          menuParametreMonstre();
+            break;
+        case 3:
+        menuParametreMonstre();
+            break;
+        case 4:
+            menuParametre();
+            break;
+        case 5:
+
+            return;
+
+        default:;
+
+
+    }
+ 
+    return;
+
+}
+
+void menuParametreObjet(){
+	int choix;
+   
+    do {
+    system("clear");
+        printf("\nAJOUTER => 1\n");
+        printf("SUPPRIMER => 2\n");
+        printf("MODIF => 3\n");
+        printf("RETOUR => 4\n");
+        printf("QUITTER => 5\n");
+         printf("Votre choix =>:");
+        scanf("%d", &choix);
+    }while(choix != 1 && choix != 2 && choix != 3 && choix != 4 && choix != 5);
+system("clear");
+    switch( choix )
+    {
+
+        case 1:
+            menuParametreObjet
+            break;
+        case 2:
+           menuParametreObjet
+            break;
+         case 3:
+           menuParametreObjet
+            break;
+    	case 4:
+           // 
+           menuParametre();
+            break;
+        case 5:
+
+            return;
+
+        default:;
+
+
+    }
+
+  
+    return;
+
+}
+
+void menuParametreRoom(){
+	int choix;
+    system("clear");
+    do {
+    system("clear");
+        printf("\nAJOUTER => 1\n");
+        printf("SUPPRIMER => 2\n");
+        printf("MODIF => 3\n");
+        printf("RETOUR => 4\n");
+        printf("QUITTER => 5\n");
+        printf("Votre choix =>:");
+        scanf("%d", &choix);
+    }while(choix != 1 && choix != 2 && choix != 3 && choix != 4 && choix != 5);
+  system("clear");
+    switch( choix )
+    {
+
+        case 1:
+        // fonc
+          menuParametreRoom()
+            break;
+        case 2:
+         menuParametreRoom()
+            break;
+         case 3:
+          menuParametreRoom()
+            break;
+          case 4:
+            menuParametre();
+            break;
+        case 5:
+
+            return;
+
+        default:;
+
+
+    }
+
+  
+    return;
+
+}
---
+++ testmenu/menu.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef MENU
+#define MENU
+
+void menuParametreRoom();
+void menuParametreObjet(); // EN DEVELOPEMENT
+void menuParametreMonstre(); // EN DEVELOPEMENT
+void menuParametreRoom();
+
+#endif
